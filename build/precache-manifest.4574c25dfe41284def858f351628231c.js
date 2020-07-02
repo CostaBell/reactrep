eaf1ff07f4671e58bcf3c743c791600ac7e03770
CostaBell <pana101@gmail.com>
2020-07-02 20:20:31 +0300

    myapp

---
+++ build/precache-manifest.4574c25dfe41284def858f351628231c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71d1e09e223791762289b4c246c0bd1e",
+    "url": "/reactrep/index.html"
+  },
+  {
+    "revision": "156b4aff203e367f1503",
+    "url": "/reactrep/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2d4d6108ff94901b8f01",
+    "url": "/reactrep/static/js/2.72dfa0a7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactrep/static/js/2.72dfa0a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "156b4aff203e367f1503",
+    "url": "/reactrep/static/js/main.9d62f0bd.chunk.js"
+  },
+  {
+    "revision": "d81e3c86019a90e4ede7",
+    "url": "/reactrep/static/js/runtime-main.af593363.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        
+        <Link to='/'>HomePage</Link>
+        <Link to='page2'>Page2</Link>
+
+        <Switch>
+
+          <Route exact path='/' component= {()=> { return <h1>HomePage Works!! </h1> }} />
+          <Route exact path='Page2' component= {()=> { return <h1>Page 2 Works!! </h1> }} />
+
+        </Switch>
+
+      </Router>
+
+
+    </div>
+  );
+}
+
+export default App;
